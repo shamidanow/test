cedce63a8a4fc32522aff217c029f7b34d1436f8
shamidanow <shamidanow@users.noreply.github.com>
2019-09-09 11:48:00 +0500

    CreateCityTable

--- database/migrations/2019_09_03_113430_create_city_table.php
+++ database/migrations/2019_09_03_113430_create_city_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateCitiesTable extends Migration
+class CreateCityTable extends Migration
 {
     /**
      * Run the migrations.
